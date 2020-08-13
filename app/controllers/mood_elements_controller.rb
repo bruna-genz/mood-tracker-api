29e8937261cbbe84c0663c367cbcfa39aa9aade6
Bruna <brunagenz92@gmail.com>
2020-08-13 19:36:52 -0300

    Feat: create mood controller

---
+++ app/controllers/mood_elements_controller.rb
@@ -0,0 +1,41 @@
+class MoodElementsController < ApplicationController
+  def create
+    @mood_element = MoodElement.new(mood_element_params)
+    if @mood_element.save
+      render json: {
+        status: "created",
+        mood_element: @mood_element
+      }
+    else
+      render json: {
+        status: 500,
+        errors: @user.errors.full_messages
+      }
+    end
+  end
+
+  def index
+    @mood_elements = MoodElement.all
+    if @mood_elements
+      render json: {
+        mood_elements: @mood_elements
+      }
+    else
+      render json: {
+        status: 500,
+        emood_elements: ['no mood elements found']
+      }
+    end
+  end
+
+  def destroy
+    @mood_element = MoodElement.find(params[:id])
+    @mood_element.destroy
+  end
+
+  private
+
+  def mood_element_params
+    params.require(:mood_element).permit(:name)
+  end
+end
---
+++ app/helpers/mood_elements_helper.rb
@@ -0,0 +1,2 @@
+module MoodElementsHelper
+end
