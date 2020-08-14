1e4973b96c822951c92e7af59e49daffd4cfbdb6
Bruna <brunagenz92@gmail.com>
2020-08-14 19:44:23 -0300

    Feat: add input_type to mood_elements

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :users, only: [:create, :show, :index]
   resources :evaluations, only: [:create, :index, :destroy]
+  resources :mood_elements, only: [:index]
 
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
