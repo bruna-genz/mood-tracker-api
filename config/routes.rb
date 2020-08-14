ab7db58a588b8d8a97e75596719303f749f68383
Bruna <brunagenz92@gmail.com>
2020-08-14 14:55:13 -0300

    Feat: add routes for evaluation controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :users, only: [:create, :show, :index]
+  resources :evaluations, only: [:create, :index, :destroy]
 
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
