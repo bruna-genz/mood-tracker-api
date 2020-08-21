fe2f9d42158f5ac5de3dfe4c89b2732c55709f3b
Bruna <brunagenz92@gmail.com>
2020-08-21 16:47:14 -0300

    Fix: edit session cookies domain to test production

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production' 
-  Rails.application.config.session_store :cookie_store, key: '_mood-tracker-api', domain: 'https://ohmymood.herokuapp.com/'
+  Rails.application.config.session_store :cookie_store, key: '_mood-tracker-api', domain: 'http://localhost:3000'
 else
   Rails.application.config.session_store :cookie_store, key: '_mood-tracker-api' 
 end
