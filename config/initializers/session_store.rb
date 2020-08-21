9f3f008558c957181476610828e082234c8ded5d
Bruna <brunagenz92@gmail.com>
2020-08-21 18:36:21 -0300

    fix: typo

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production' 
-  Rails.application.config.session_store :cookie_store, key: '_mood-tracker-api', domain: 'https://ohmymood.netlify.app/'
+  Rails.application.config.session_store :cookie_store, key: '_mood-tracker-api', domain: 'https://ohmymood.netlify.app'
 else
   Rails.application.config.session_store :cookie_store, key: '_mood-tracker-api' 
 end
