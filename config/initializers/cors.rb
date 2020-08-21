26b6bcd016f30dfddf0a0f7f0f2c7748c9a41952
Bruna <brunagenz92@gmail.com>
2020-08-21 17:53:52 -0300

    Fix: change url on CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
-    origins 'https://ohmymood.herokuapp.com/'
+    origins 'https://ohmymood.netlify.app/'
   
     resource '*',
       headers: :any,
