e43e6973583dacadef3773803462961a01019755
Bruna <brunagenz92@gmail.com>
2020-08-28 17:04:37 -0300

    Fix: remove domain to test

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production' 
-  Rails.application.config.session_store :cookie_store, key: '_mood-tracker-api', domain: '.herokuapp.com', secure: true, same_site: :none
+  Rails.application.config.session_store :cookie_store, key: '_mood-tracker-api', secure: true, same_site: :none
 else
   Rails.application.config.session_store :cookie_store, key: '_mood-tracker-api' 
 end
