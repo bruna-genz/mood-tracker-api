2e0189ed2123943af0c7697a260dd3ab09b20c5d
Bruna <brunagenz92@gmail.com>
2020-08-28 14:35:00 -0300

    Fix: edit session_store config

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,6 @@
 if Rails.env === 'production' 
-  Rails.application.config.session_store :cookie_store, key: '_mood-tracker-api', domain: 'https://mood-tracker-api.herokuapp.com', secure: true, same_site: :lax
+  Rails.application.config.session_store :cookie_store, key: '_mood-tracker-api', domain: 'https://mood-tracker-api.herokuapp.com', secure: true, same_site: :none
+  Rails.application.config.session_store :cookie_store, key: '_session_id', domain: 'https://mood-tracker-api.herokuapp.com', secure: true, same_site: :none
 else
   Rails.application.config.session_store :cookie_store, key: '_mood-tracker-api' 
 end
