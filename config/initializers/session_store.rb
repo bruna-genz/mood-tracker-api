dcf8fa649bbae492cfde6be9fb2853c012be2429
Bruna <brunagenz92@gmail.com>
2020-08-28 14:04:25 -0300

    Fix: add same_site config to session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production' 
-  Rails.application.config.session_store :cookie_store, key: '_mood-tracker-api', domain: 'https://mood-tracker-api.herokuapp.com'
+  Rails.application.config.session_store :cookie_store, key: '_mood-tracker-api', domain: 'https://mood-tracker-api.herokuapp.com', same_site: :none
 else
   Rails.application.config.session_store :cookie_store, key: '_mood-tracker-api' 
 end
