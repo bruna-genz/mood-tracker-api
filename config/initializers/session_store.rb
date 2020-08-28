b7d79e639d339b985e4ed7a2c80c06e235f09b1e
Bruna <brunagenz92@gmail.com>
2020-08-28 14:25:02 -0300

    Fix: remove rack.rb file and add configuration to session_store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production' 
-  Rails.application.config.session_store :cookie_store, key: '_mood-tracker-api', domain: 'https://mood-tracker-api.herokuapp.com'
+  Rails.application.config.session_store :cookie_store, key: '_mood-tracker-api', domain: 'https://mood-tracker-api.herokuapp.com', secure: true, same_site: :lax
 else
   Rails.application.config.session_store :cookie_store, key: '_mood-tracker-api' 
 end
