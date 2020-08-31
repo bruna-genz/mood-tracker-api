ddfd896dbb3fb9a4b77fbbb1d863c29b7fa60ef3
Bruna <brunagenz92@gmail.com>
2020-08-31 16:44:02 -0300

    Fix: add cookies configuration that I had removed earlier

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production' 
-  Rails.application.config.session_store :cookie_store, key: '_mood-tracker-api'
+  Rails.application.config.session_store :cookie_store, key: '_mood-tracker-api', secure: true, same_site: :none
 else
   Rails.application.config.session_store :cookie_store, key: '_mood-tracker-api' 
 end
