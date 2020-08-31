b1ef5f7d796281285c956adf3c1a5d934be64bfb
Bruna <brunagenz92@gmail.com>
2020-08-31 13:09:06 -0300

    Fix: typo again

--- app/controllers/evaluations_controller.rb
+++ app/controllers/evaluations_controller.rb
@@ -50,7 +50,7 @@ class EvaluationsController < ApplicationController
 
   def find_current_user
     if current_user.nil?
-      current_user = User.find(evaluations_params[:user_id])
+      current_user = User.find(evaluation_params[:user_id])
     end
   end
 end
